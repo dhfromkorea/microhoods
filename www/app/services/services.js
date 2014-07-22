5e7dc4a731d702450c85873556fb96dd38d02498
dhfromkorea <dhllee424@gmail.com>
2014-07-22 16:38:43 -0700

    refactoring 1.1 - moved state config in the respective folders - cleaned up syntax errors - reformatted the code (indentation etc.) - renamed the app var from microhoods to app, to sth more general - added app.html to move common html parent tags later on if needed

--- www/app/services/services.js
+++ www/app/services/services.js
@@ -1,25 +1,38 @@
-angular.module('app.services',[])
-.factory('fbAuth', function($state){
-  // create an authorization service, where we can store/access user data on login
-  var ref = new Firebase('https://mcrhds.firebaseio.com');
-  var service = {};
-  service.user = undefined;
-  service.auth = new FirebaseSimpleLogin(ref, function(error, user) {
-    if (error) {
-      // an error occurred while attempting login
-      console.log(error);
-    } else if (user) {
-      // user authenticated with Firebase
-      service.user = user;
-      var payload = JSON.stringify({ googleId: user.id, googleDisplayName: user.displayName });
+angular.module('app.services', [])
+  .factory('authFactory', function($state) {
+    // create an authorization service, where we can store/access user data on login
+    var ref = new Firebase('https://mcrhds.firebaseio.com');
+    var user = undefined;
+    var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+      if (error) {
+        // an error occurred while attempting login
+        console.log(error);
+      } else if (user) {
+        // user authenticated with Firebase
+        service.user = user;
+        var payload = JSON.stringify({
+          googleId: user.id,
+          googleDisplayName: user.displayName
+        });
 
-      var request = new XMLHttpRequest();
-      request.open('POST', '/', true);
-      request.send(payload);
-      $state.transitionTo('home');
-    } else {
-      // user is logged out
-    }
+        var request = new XMLHttpRequest();
+        request.open('POST', '/', true);
+        request.send(payload);
+        $state.transitionTo('home');
+      } else {
+        // user is logged out
+      }
+    });
+    return {
+      user: user,
+      auth: auth
+    };
+  })
+  .factory('mapFactory', function() {
+
+
+    return {
+
+
+    };
   });
-  return service;
-});
